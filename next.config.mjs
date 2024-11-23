6a101dc3d942fbbceb214ae6bdae6a48279f3ca4
Ash <redcoolike@gmail.com>
2024-11-23 21:13:07 +0300

    initialCommit

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export', 
+  basePath: 'unchainedraggedy', 
+  trailingSlash: true, // Добавить слеш в конце URL
 };
 
 export default nextConfig;
---
+++ src/components/PostCard.js
@@ -0,0 +1,18 @@
+import Image from "next/image";
+import styles from "@/styles/Home.module.css";
+
+export default function PostCard({ post }) {
+  return (
+    <div className={styles.postCard}>
+      <Image
+        className={styles.postImage}
+        src={post.image}
+        alt={`Post ${post.id}`}
+        width={500}
+        height={500}
+      />
+      <p>{post.description}</p>
+      <div className={styles.likes}>❤️ {post.likes} лайков</div>
+    </div>
+  );
+}
