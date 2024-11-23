606d53d58693a4083240c6cb707e96fde73a8084
Ash <redcoolike@gmail.com>
2024-11-23 21:21:47 +0300

    deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export', 
-  basePath: 'unchainedraggedy', 
-  trailingSlash: true, 
+  output: 'export',
 };
 
 export default nextConfig;
